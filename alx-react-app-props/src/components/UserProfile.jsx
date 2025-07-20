924d8c8903872c6fae3607d6ead76e81ff7e2cf7
Hanna Araya Gebremedhin <hannagebremedhin945@gmail.com>
2025-07-20 22:46:28 +0300

    Refactor UserProfile to consume UserContext using useContext hook

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,9 +1,14 @@
-const UserProfile = (props) => {
+import React, { useContext } from 'react';
+import UserContext from '../UserContext';  // adjust path if UserContext.js is in src/
+
+const UserProfile = () => {
+  const user = useContext(UserContext);  // consume context instead of props
+
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+      <h2>{user.name}</h2>
+      <p>Email: {user.email}</p>
+      {/* Add age and bio if needed and available */}
     </div>
   );
 };
