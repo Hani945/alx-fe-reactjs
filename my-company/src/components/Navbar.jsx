e1158a5606cef8f99d32d97553647f8a924c9d06
Hanna Araya Gebremedhin <hannagebremedhin945@gmail.com>
2025-07-20 21:54:39 +0300

    Add display and justifyContent inline styles to Navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,14 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ backgroundColor: '#333', padding: '10px' }}>
+    <nav
+      style={{
+        display: 'flex',
+        justifyContent: 'space-around',
+        backgroundColor: '#333',
+        padding: '10px',
+      }}
+    >
       <Link to="/" style={{ color: 'white', margin: '10px' }}>Home</Link>
       <Link to="/about" style={{ color: 'white', margin: '10px' }}>About</Link>
       <Link to="/services" style={{ color: 'white', margin: '10px' }}>Services</Link>
