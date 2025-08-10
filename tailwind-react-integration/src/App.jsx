e6f5bcc1907a55b296071fe27acd15391405ae69
Hanna Araya Gebremedhin <hannagebremedhin945@gmail.com>
2025-08-10 15:54:31 +0300

    Add Tailwind CSS integration project with responsive and interactive UserProfile component

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
