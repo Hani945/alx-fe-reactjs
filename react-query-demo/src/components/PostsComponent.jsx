600db6c3b12f3474c0b504c504a58efacf9e1203
Hanna Araya Gebremedhin <hannagebremedhin945@gmail.com>
2025-08-24 20:01:35 +0300

    last query

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -8,10 +8,10 @@ const fetchPosts = async () => {
 };
 
 const PostsComponent = () => {
-  const { data, error, isLoading, refetch } = useQuery(["posts"], fetchPosts);
+  const { data, error, isLoading, isError, refetch } = useQuery(["posts"], fetchPosts);
 
   if (isLoading) return <p>Loading posts...</p>;
-  if (error) return <p>Error loading posts: {error.message}</p>;
+  if (isError) return <p>Error loading posts: {error.message}</p>;
 
   return (
     <div>
