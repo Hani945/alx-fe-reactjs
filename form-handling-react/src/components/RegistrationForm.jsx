55c42caa6ee77a546dc6d809c5fc0fac0dc9dded
Hanna Araya Gebremedhin <hannagebremedhin945@gmail.com>
2025-08-24 11:29:56 +0300

    Updated form-handling

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -4,22 +4,32 @@ export default function RegistrationForm() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const [errors, setErrors] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!username || !email || !password) {
-      setError("All fields required");
+
+    if (!username) {
+      setErrors("Username required");
       return;
     }
-    setError("");
+    if (!email) {
+      setErrors("Email required");
+      return;
+    }
+    if (!password) {
+      setErrors("Password required");
+      return;
+    }
+
+    setErrors("");
     alert(`User Registered: ${username}, ${email}`);
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <h2>Controlled Registration Form</h2>
-      {error && <p style={{ color: "red" }}>{error}</p>}
+      {errors && <p style={{ color: "red" }}>{errors}</p>}
       <input
         type="text"
         placeholder="Username"
